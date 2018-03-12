This is a mess but SVM rbf multiclass abalone works, at least on sklearn. fixed normalization bug.

ml_lib/svm.py
@@ -8,15 +8,19 @@ from __future__ import division
 import numpy as np
 from cvxopt import matrix, solvers
 
-from sklearn import svm
+# from sklearn import svm
 
 class LinearKernel:
+    def __init__(self):
+        print "Linear kernel"
+
     def compute(self, X1, X2):
         ''' Creates a linear kernel matrix between rows of X1 and X2. '''
         return np.dot(X1, X2.T)
 
 class RBFKernel:
     def __init__(self, width):
+        print "RBF kernel"
         self.width = width
 
     def compute(self, X1, X2):
@@ -41,6 +45,7 @@ class SVM(object):
         self.X = X
         self.Y = Y
         self.lam = lam
+        print "SVM with lambda = %f" %(lam)
         
         self.alpha = None
         self.w0 = None
@@ -55,8 +60,8 @@ class SVM(object):
         Y = self.Y * 2 - 1
         Y_ = Y.reshape((n, 1))
 
-        self.lin_clf = svm.LinearSVC(loss='hinge')
-        self.lin_clf.fit(X, Y)
+#         self.lin_clf = svm.LinearSVC(loss='hinge')
+#         self.lin_clf.fit(X, Y)
         
         K = self.kernel.compute(X, X)
         
@@ -72,6 +77,8 @@ class SVM(object):
         A = matrix(Y.reshape((1, n)).astype(float))
         b = matrix([0.0])
         
+        print "...Learning..."
+        solvers.options['show_progress'] = False
         sol = solvers.qp(P, q, G, h, A, b)
 
         al = np.reshape(np.array(sol['x']), (n))
@@ -101,7 +108,7 @@ class SVM(object):
         """
         if self.alpha is None: self.learn()
 
-        dec = self.lin_clf.decision_function(X_test)
+#         dec = self.lin_clf.decision_function(X_test)
 
         print "w0: %f" %(self.w0)
 
@@ -113,13 +120,13 @@ class SVM(object):
         Kmn = self.kernel.compute(X0, X)
         ret = np.dot(Kmn, self.alpha * Y) + self.w0
         
-        df =  (ret - dec)
-        d2 = np.zeros(df.shape[0])
-        d2[df>0] = 1
-        print "Diff between Y predictions:"
-        print "  Average : %f off from %f" % (np.average(df), np.average(dec))
-        print "  Ratio   : %f " % (np.average(df / dec))
-        print "  #Greater: %s / %s" % (np.sum(d2), X_test.shape[0])
+#         df =  (ret - dec)
+#         d2 = np.zeros(df.shape[0])
+#         d2[ret*dec > 0] = 1
+#         print "Diff between Y predictions:"
+#         print "  Average : %f off from %f" % (np.average(df), np.average(dec))
+#         print "  Ratio   : %f " % (np.average(df / dec))
+#         print "  #match: %s / %s" % (np.sum(d2), X_test.shape[0])
         
         return ret
 

ml_lib/util.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import time
 import heapq as hq
 
-
 # ------ Algorithm helpers ---------
 
 
@@ -36,8 +35,8 @@ def normalize(X, f_range=None, f_mean=None):
 def linear_multiclassify(X, Ya, X_test, Ya_test, split_points,
                          create_classifier):
     n = len(split_points)
-    print "Running linear multiclassifier on %s splits" %(n)
-    print split_points
+#     print "Running linear multiclassifier on %s splits -----" %(n)
+#     print split_points
     
     Yp = np.zeros((Ya_test.shape[0], n))
     for i, spl in enumerate(split_points):
@@ -48,6 +47,9 @@ def linear_multiclassify(X, Ya, X_test, Ya_test, split_points,
         classifier = create_classifier(X, Yb)
         print classifier.classify(X, Yb)
 
+        Yb_test = np.zeros((Ya_test.shape[0]))
+        Yb_test[Ya_test>=i] = 1
+        report_accuracy(classifier.classify(X_test, Yb_test))
         Yp_i = classifier.predict(X_test)
         Yp[:, i] = Yp_i
         
@@ -73,6 +75,107 @@ def linear_multiclassify(X, Ya, X_test, Ya_test, split_points,
                 np.logical_and((Yguess == j), (Ya_test == i)))
     print 'Overall test acc: %f%%' % get_accuracy(c_matrix)
     print c_matrix
+    return c_matrix
+
+def onevsall_multiclassify(X, Ya, X_test, Ya_test, n, create_classifier):
+#     print "Running linear multiclassifier on %s splits -----" %(n)
+#     print split_points
+    
+    Yp = np.zeros((Ya_test.shape[0], n))
+    for i in range(n):
+        Yb = np.zeros((Ya.shape[0]))
+        Yb[Ya==i] = 1
+
+        classifier = create_classifier(X, Yb)
+        print classifier.classify(X, Yb)
+        
+        Yb_test = np.zeros((Ya_test.shape[0]))
+        Yb_test[Ya_test==i] = 1
+        print classifier.classify(X_test, Yb_test)
+
+        Yp_i = classifier.predict(X_test)
+        Yp[:, i] = Yp_i
+        
+#     print Yp.shape
+#     print Yp
+    Yguess = np.argmax(Yp, axis=1)
+
+    c_matrix = confusion_matrix(Ya_test, Yguess, n)    
+
+    print 'Overall test acc: %f%%' % get_accuracy(c_matrix)
+    print c_matrix
+    print "......."
+    print "......."
+    print "......."
+    
+    return c_matrix
+
+def onevsall_multiclassify_validation(X, Y, X_val, Y_val, n,
+                                      create_classifier_validated,
+                                      var1_list, var2_list):
+#     print "Running linear multiclassifier on %s splits -----" %(n)
+#     print split_points
+    
+    Yp = np.zeros((Y_val.shape[0], n))
+    
+    for i in range(n):
+        Yb = np.zeros((Y.shape[0]))
+        Yb[Y==i] = 1
+        Yb_val = np.zeros((Y_val.shape[0]))
+        Yb_val[Y_val==i] = 1
+        
+        max_accuracy = -1
+        best_classifier = None
+        # Try all variations on the variables
+        for var1 in var1_list:
+            print "============== var1 = %f ===========" %(var1)
+            for var2 in var2_list:
+                print "-------------- var2 = %f ----------" %(var2)
+                classifier = create_classifier_validated(var1, var2, X, Yb)
+                #print classifier.classify(X, Yb)
+                
+                cm_v = classifier.classify(X_val, Yb_val)
+                acc_v = get_accuracy(cm_v)
+                if acc_v > max_accuracy:
+                    max_accuracy = acc_v
+                    best_classifier = classifier
+                    print "*** Found better with %f%%" % acc_v
+                
+        print
+        print "============== DONE class %d ===============" % (i)
+        print
+        print "Best:"
+        print best_classifier.classify(X_val, Yb_val)
+        Yp_i = best_classifier.predict(X_val)
+        Yp[:, i] = Yp_i
+        
+    print "============== DONE ALL ==============="
+    print
+    Yguess = np.argmax(Yp, axis=1)
+
+    c_matrix = confusion_matrix(Y_val, Yguess, n)    
+
+    print 'Overall test acc: %f%%' % get_accuracy(c_matrix)
+    print c_matrix
+    print "......."
+    print "......."
+    print "......."
+    
+    return c_matrix
+
+
+# def find_best_setting(param_options, fn):
+#     max = float("-inf")
+#     max_param = None
+#     for param in param_options:
+#         ret = fn(param)
+#         if ret > max:
+#             max = ret
+#             max_param = param
+#     return (max, max_param)
+
+# def validate_settings(X, Y, X_test, Y_test, create_classifier):
+    
 
 # ------ Drawing ---------
 
@@ -89,21 +192,26 @@ def prefix():
         ("%s_"%pre_alg if pre_alg else '') + \
         ("%s_"%pre_norm if pre_norm else '')
 
-def split_into_train_test_sets(X, Y, test_portion):
-    # Split into Training and Testing sets    
+def split_into_train_test_sets(X, Y, test_portion, validation_portion):
+    # Split into Training and Testing sets
     pre_portion = test_portion
     train_idx=[]
     test_idx=[]
+    valid_idx=[]
     for i in range(X.shape[0]):
         if i % 4 == test_portion:
             test_idx.append(i)
+        elif (validation_portion is not None) and i % 4 == validation_portion:
+            valid_idx.append(i)
         else:
             train_idx.append(i)
     X_test = X[test_idx]
     Y_test = Y[test_idx]
+    X_valid = X[valid_idx]
+    Y_valid = Y[valid_idx]
     X = X[train_idx]
     Y = Y[train_idx]
-    return (X, Y, X_test, Y_test)
+    return (X, Y, X_test, Y_test, X_valid, Y_valid)
 
 def draw_class_histograms(X, Y, num_classes, col):
     for c in range(num_classes):
@@ -211,6 +319,16 @@ def draw_classes_data(X, Y, colA, colB):
     plt.show()
     return plt
 
+def plot_accuracy(acc, x_values, line_labels=None, pref=None):
+    plt.plot(x_values, acc, 'b-')
+    plt.xlabel('C or lambda')
+    plt.ylabel('Test accuracy')
+    fname = prefix() \
+        + ("%s_"%pref if pref else '') \
+        + 'val.png'
+    plt.savefig(fname, bbox_inches='tight')
+    print fname
+    plt.show()
 
 # ------ Logging/Debugging ---------
 
@@ -225,3 +343,10 @@ def get_accuracy(c_matrix):
     
     return 100 * (correct / np.sum(c_matrix))
     
+def confusion_matrix(Y_actual, Y_guess, n):
+    c_matrix = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            c_matrix[i, j] = np.sum(
+                np.logical_and((Y_guess == j), (Y_actual == i)))
+    return c_matrix

uci/abalone.py
@@ -2,6 +2,7 @@
 @author erw
 
 '''
+from __future__ import division
 
 import argparse
 import numpy as np
@@ -10,6 +11,9 @@ from ml_lib.gaussian_plugin_classifier import GaussianPlugInClassifier
 from ml_lib.gaussian_naive_classifier import GaussianNaiveClassifier
 
 import ml_lib.util as util
+import math 
+import time
+
 
 # Draw histograms for each column
 def draw_classes_histogram(X, Y, num_classes):
@@ -25,6 +29,9 @@ if __name__ == '__main__':
     parser.add_argument('--test_portion',
                         help='Which portion to use as test set',
                         default=1, type=int)
+    parser.add_argument('--validation_portion',
+                        help='Which portion to use as validation set',
+                        default=2, type=int)
     parser.add_argument('--draw_classes_data', action='store_true')
     parser.add_argument('--draw_classes_histogram', action='store_true')
     parser.add_argument('--normalize', action='store_true')
@@ -67,18 +74,20 @@ if __name__ == '__main__':
     print "  Females: ", np.sum(X[:,1])
     print "  Infants: ", np.sum(X[:,2])
     
-    X, Y, X_test, Y_test = util.split_into_train_test_sets(
-        X, Y, args.test_portion)
+    X, Y, X_test, Y_test, X_valid, Y_valid = util.split_into_train_test_sets(
+        X, Y, args.test_portion, args.validation_portion)
     
     print "Training dataset:   %s" % (X.shape, )
     print "Testing dataset(%d): %s" % (args.test_portion, X_test.shape)
+    print "Validation dataset(%d): %s" % (args.validation_portion,
+                                          X_valid.shape)
 
     if args.normalize:
         print "Normalizing..."
         util.pre_norm = "n"
         X, f_range, f_mean = util.normalize(X)
         X_test = util.normalize(X_test, f_range, f_mean)[0]
-
+        X_valid = util.normalize(X_valid, f_range, f_mean)[0]
 
     if True: # Classification
         split_points = [-1, 8, 10]
@@ -94,10 +103,19 @@ if __name__ == '__main__':
         for i, spl in enumerate(split_points):
             Ya_test[Y_test>spl] = i
 
+        Ya_valid = np.zeros(Y_valid.shape)
+        for i, spl in enumerate(split_points):
+            Ya_valid[Y_valid>spl] = i
+
         print "Training set"
         print "  Class 0: ", np.sum(Ya == 0)
         print "  Class 1: ", np.sum(Ya == 1)
         print "  Class 2: ", np.sum(Ya == 2)
+
+        print "Validation set"
+        print "  Class 0: ", np.sum(Ya_valid == 0)
+        print "  Class 1: ", np.sum(Ya_valid == 1)
+        print "  Class 2: ", np.sum(Ya_valid == 2)
 
         if args.draw_classes_histogram:
             draw_classes_histogram(X, Ya, n)
@@ -163,15 +181,70 @@ if __name__ == '__main__':
             print "Support Vector Machine..."
             util.pre_alg = "svm"
             from ml_lib.svm import SVM, RBFKernel
+            from ml_lib.svm_sk_svc import SVMSkSVC
+            from ml_lib.svm_sk_linear import SVMSkLinear
 
-            def create_classifier(X, Y):
-                return SVM(X, Y, 10)
-                 
 #             Yb = np.zeros((Ya.shape[0]))
 #             Yb[Ya>=i] = 1
 #             svm_classifier = SVM(X, Yb, 1)#), kernel=RBFKernel(1))
 #             util.report_accuracy(svm_classifier.classify(X, Yb))
 #             util.report_accuracy(svm_classifier.classify(X_test, Y_test))
+  
+#             util.linear_multiclassify(X, Ya, X_valid, Ya_valid, split_points, 
+#                                       lambda X, Y, lam=1: SVMSkLinear(X, Y, lam))
+            
+#             cm = util.onevsall_multiclassify(
+# #                         X, Ya, X, Ya, len(split_points),
+#                 X, Ya, X_valid, Ya_valid, len(split_points),
+#                 lambda X, Y, lam=1000, b=0.1: SVMSkSVC(X, Y, lam, b,
+#                                                     kernel='rbf'))
+#             util.report_accuracy(cm)
+            
+            lam_val = [math.pow(2, p-3) for p in range(8)]                 
+#             lam_val = [(p+1)/10 for p in range(8)]
+            b_val = [(p+1)/10 for p in range(10)]
 
-            util.linear_multiclassify(X, Ya, X_test, Ya_test,
-                                       split_points, create_classifier)
+            cm = util.onevsall_multiclassify_validation(
+                X, Ya, X_valid, Ya_valid, len(split_points),
+                lambda lam, b, X, Y: SVMSkSVC(X, Y, lam, b, kernel='rbf'),
+                lam_val, b_val)
+     
+#             acc = np.zeros((len(lam_val), len(b_val)))
+#             acc2 = np.zeros((len(lam_val), len(b_val)))
+#             for i, lam in enumerate(lam_val):
+#                 print "============== lam = %f ===========" %(lam)
+#                 for j, b in enumerate(b_val):
+#                     print "-------------- b = %f ----------" %(b)
+# #                     cm = util.onevsall_multiclassify(
+# #                         X, Ya, X_valid, Ya_valid, len(split_points),
+# #                         lambda X, Y, lam=lam, b=b: SVM(X, Y, lam,
+# #                                                        kernel=RBFKernel(b)))
+# #                     acc[i,j] = util.get_accuracy(cm)
+#                     cm = util.onevsall_multiclassify(
+# #                         X, Ya, X, Ya, len(split_points),
+#                         X, Ya, X_valid, Ya_valid, len(split_points),
+#                         lambda X, Y, lam=lam, b=b: SVMSkSVC(X, Y, lam, b,
+#                                                             kernel='rbf'))
+#  
+# #                     Yb = np.zeros((Ya.shape[0]))
+# #                     Yb[Ya==i] = 1
+# #                     classifier = create_classifier(X, Yb)
+# #                     print classifier.classify(X, Yb)
+# #                     Yp_i = classifier.predict(X_test)
+#                      
+#                     acc2[i,j] = util.get_accuracy(cm)
+#      
+#             print "\nAccuracies found for lambda:"
+#             print lam_val
+#             print b_val
+#             print acc
+#             print acc2
+#             tym = str(time.time() % 1000000)
+#             np.savetxt(util.prefix() + tym + "_cv.csv", acc,
+#                        delimiter=",", fmt='%.3f')
+#             np.savetxt(util.prefix() + tym + "_2_cv.csv", acc2,
+#                        delimiter=",", fmt='%.3f')
+# #             for i, lam in enumerate(lam_val):
+# #                 for j, b in enumerate(b_val):
+# #                     print "%f, %d: \t%f" %(lam, b, acc[i])
+# #             util.plot_accuracy(acc, lam_val)

uci/wdbc.py
@@ -7,7 +7,7 @@ from __future__ import division
 import numpy as np
 import ml_lib.util as util
 import argparse
-
+import math
 
 # Draw histograms for each column
 def draw_classes_histogram(X, Y):
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     parser.add_argument('--output_dir', help='path to store output files')
     parser.add_argument('--test_portion',
                         help='Which portion to use as test set',
-                        default=2, type=int)
+                        default=1, type=int)
     parser.add_argument('--draw_classes_data', action='store_true')
     parser.add_argument('--draw_classes_histogram', action='store_true')
     parser.add_argument('--normalize', action='store_true')
@@ -172,6 +172,19 @@ if __name__ == '__main__':
         util.pre_alg = "svm"
         from ml_lib.svm import SVM, RBFKernel
 
-        svm_classifier = SVM(X, Y, 1, kernel=RBFKernel(1))
-        util.report_accuracy(svm_classifier.classify(X_test, Y_test))
+        lam_val = []
+        for lam_pow in range(20):
+            lam_val.append(math.pow(1.2, lam_pow))
 
+        acc = np.zeros(len(lam_val))
+        for i, lam in enumerate(lam_val):
+            svm_classifier = SVM(X, Y, lam)#), kernel=RBFKernel(1))
+            #util.report_accuracy(svm_classifier.classify(X, Y))
+            cm = svm_classifier.classify(X_test, Y_test)
+            util.report_accuracy(cm)
+            acc[i] = util.get_accuracy(cm)
+
+        print "\nAccuracies found for lambda:"
+        for i, lam in enumerate(lam_val):
+            print "%f: \t%f" %(lam, acc[i])
+        util.plot_accuracy(acc, lam_val)